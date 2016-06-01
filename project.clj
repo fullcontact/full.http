fullcontact/full.rabbit

Version 0.4.1-SNAPSHOT

project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.rabbit "0.4.0"
+(defproject fullcontact/full.rabbit "0.4.1-SNAPSHOT"
   :description "RabbitMQ sugar on top of langohr."
 
   :dependencies [[com.novemberain/langohr "3.0.0-rc2"]

fullcontact/full.http

project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.http "0.4.0"
+(defproject fullcontact/full.http "0.4.1-SNAPSHOT"
   :description "Async HTTP client and server on top of http-kit and core.async."
 
   :dependencies [[http-kit "2.1.18"]