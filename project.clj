fullcontact/full.bootstrap

Version 0.4.11-SNAPSHOT

project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.bootstrap "0.4.10"
+(defproject fullcontact/full.bootstrap "0.4.11-SNAPSHOT"
   :description "Boostrap module that pulls in all commonly used full-monty dependencies."
 
   :url "https://github.com/fullcontact/full.monty"
@@ -10,15 +10,15 @@
   :deploy-repositories [["releases" {:url "https://clojars.org/repo/" :creds :gpg}]]
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [fullcontact/full.core "0.4.10"]
-                 [fullcontact/full.time "0.4.10"]
-                 [fullcontact/camelsnake "0.4.10"]
-                 [fullcontact/full.json "0.4.10"]
-                 [fullcontact/full.async "0.4.10"]
-                 [fullcontact/full.dev "0.4.10"]
-                 [fullcontact/full.cache "0.4.10"]
-                 [fullcontact/full.metrics "0.4.10"]
-                 [fullcontact/full.http "0.4.10"]]
+                 [fullcontact/full.core "0.4.11-SNAPSHOT"]
+                 [fullcontact/full.time "0.4.11-SNAPSHOT"]
+                 [fullcontact/camelsnake "0.4.11-SNAPSHOT"]
+                 [fullcontact/full.json "0.4.11-SNAPSHOT"]
+                 [fullcontact/full.async "0.4.11-SNAPSHOT"]
+                 [fullcontact/full.dev "0.4.11-SNAPSHOT"]
+                 [fullcontact/full.cache "0.4.11-SNAPSHOT"]
+                 [fullcontact/full.metrics "0.4.11-SNAPSHOT"]
+                 [fullcontact/full.http "0.4.11-SNAPSHOT"]]
 
   :aot :all
 

fullcontact/full.http

project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.http "0.4.10"
+(defproject fullcontact/full.http "0.4.11-SNAPSHOT"
   :description "Async HTTP client and server on top of http-kit and core.async."
 
   :url "https://github.com/fullcontact/full.monty"
@@ -13,11 +13,11 @@
                  [http-kit "2.1.18"]
                  [compojure "1.3.1"]
                  [javax.servlet/servlet-api "2.5"]
-                 [fullcontact/camelsnake "0.4.10"]
-                 [fullcontact/full.json "0.4.10"]
-                 [fullcontact/full.metrics "0.4.10"]
-                 [fullcontact/full.async "0.4.10"]
-                 [fullcontact/full.core "0.4.10"]]
+                 [fullcontact/camelsnake "0.4.11-SNAPSHOT"]
+                 [fullcontact/full.json "0.4.11-SNAPSHOT"]
+                 [fullcontact/full.metrics "0.4.11-SNAPSHOT"]
+                 [fullcontact/full.async "0.4.11-SNAPSHOT"]
+                 [fullcontact/full.core "0.4.11-SNAPSHOT"]]
 
   :aot :all
 