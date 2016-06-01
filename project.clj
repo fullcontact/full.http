fullcontact/full.bootstrap

clojure 1.7.0, version -> 0.7.0-SNAPSHOT

project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.bootstrap "0.5.10-SNAPSHOT"
+(defproject fullcontact/full.bootstrap "0.7.0-SNAPSHOT"
   :description "Boostrap module that pulls in all commonly used full-monty dependencies."
 
   :url "https://github.com/fullcontact/full.monty"
@@ -9,16 +9,16 @@
 
   :deploy-repositories [["releases" {:url "https://clojars.org/repo/" :creds :gpg}]]
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [fullcontact/full.core "0.5.10-SNAPSHOT"]
-                 [fullcontact/full.time "0.5.10-SNAPSHOT"]
-                 [fullcontact/camelsnake "0.5.10-SNAPSHOT"]
-                 [fullcontact/full.json "0.5.10-SNAPSHOT"]
-                 [fullcontact/full.async "0.5.10-SNAPSHOT"]
-                 [fullcontact/full.dev "0.5.10-SNAPSHOT"]
-                 [fullcontact/full.cache "0.5.10-SNAPSHOT"]
-                 [fullcontact/full.metrics "0.5.10-SNAPSHOT"]
-                 [fullcontact/full.http "0.5.10-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [fullcontact/full.core "0.7.0-SNAPSHOT"]
+                 [fullcontact/full.time "0.7.0-SNAPSHOT"]
+                 [fullcontact/camelsnake "0.7.0-SNAPSHOT"]
+                 [fullcontact/full.json "0.7.0-SNAPSHOT"]
+                 [fullcontact/full.async "0.7.0-SNAPSHOT"]
+                 [fullcontact/full.dev "0.7.0-SNAPSHOT"]
+                 [fullcontact/full.cache "0.7.0-SNAPSHOT"]
+                 [fullcontact/full.metrics "0.7.0-SNAPSHOT"]
+                 [fullcontact/full.http "0.7.0-SNAPSHOT"]]
 
   :aot :all
 

fullcontact/full.http

project.clj
@@ -1,4 +1,4 @@
-(defproject fullcontact/full.http "0.5.10-SNAPSHOT"
+(defproject fullcontact/full.http "0.7.0-SNAPSHOT"
   :description "Async HTTP client and server on top of http-kit and core.async."
 
   :url "https://github.com/fullcontact/full.monty"
@@ -9,16 +9,16 @@
 
   :deploy-repositories [["releases" {:url "https://clojars.org/repo/" :creds :gpg}]]
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [http-kit "2.1.18"]
-                 [compojure "1.3.1"]
+                 [compojure "1.3.4" :exclusions [clj-time]]
                  [javax.servlet/servlet-api "2.5"]
                  [ring-cors "0.1.7"]
-                 [fullcontact/camelsnake "0.5.10-SNAPSHOT"]
-                 [fullcontact/full.json "0.5.10-SNAPSHOT"]
-                 [fullcontact/full.metrics "0.5.10-SNAPSHOT"]
-                 [fullcontact/full.async "0.5.10-SNAPSHOT"]
-                 [fullcontact/full.core "0.5.10-SNAPSHOT"]]
+                 [fullcontact/camelsnake "0.7.0-SNAPSHOT"]
+                 [fullcontact/full.json "0.7.0-SNAPSHOT"]
+                 [fullcontact/full.metrics "0.7.0-SNAPSHOT"]
+                 [fullcontact/full.async "0.7.0-SNAPSHOT"]
+                 [fullcontact/full.core "0.7.0-SNAPSHOT"]]
 
   :aot :all
 