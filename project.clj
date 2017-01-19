3eec60bbd895cf1fc5c46790ec1100c025103ff4
zarlen <zach@fullcontact.com>
2017-01-19 12:07:13 -0700

    Project: full.metrics -> 0.11.4

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
                  [ring-cors "0.1.7"]
                  [fullcontact/camelsnake "0.9.0"]
                  [fullcontact/full.json "0.10.1"]
-                 [fullcontact/full.metrics "0.11.3"]
+                 [fullcontact/full.metrics "0.11.4"]
                  [fullcontact/full.async "0.9.0"]
                  [fullcontact/full.core "0.10.1"]]
   :aot :all
